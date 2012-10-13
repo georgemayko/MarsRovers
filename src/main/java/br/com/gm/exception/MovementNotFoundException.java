632cbd6ef18d56e8c62b49a4e57375da24776796
George Mayko <mayko.george@gmail.com>
2012-10-13 20:58:39 -0300

    creating new exception on convert char to Movement Enum

---
+++ src/main/java/br/com/gm/exception/MovementNotFoundException.java
@@ -0,0 +1,18 @@
+package br.com.gm.exception;
+
+public class MovementNotFoundException extends Exception {
+	
+	
+	private static final long serialVersionUID = -7250433822032634281L;
+	private final static String MESSAGE = "Can not convert movement into enum";
+	
+	public MovementNotFoundException() {
+		
+	}
+	@Override
+	public String getMessage() {
+		//return super.getMessage();
+		return MESSAGE;
+	}
+
+}
