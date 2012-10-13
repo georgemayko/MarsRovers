d7ec8752b72eb4ad7fe49a7a7ce8c0776104fede
George Mayko <mayko.george@gmail.com>
2012-10-13 20:24:34 -0300

    executing minimum for application

--- src/main/java/br/com/gm/App.java
+++ src/main/java/br/com/gm/App.java
@@ -1,18 +1,30 @@
 package br.com.gm;
 
+import br.com.gm.exception.CreatingRoverException;
 import br.com.gm.model.MarsRover;
 
-/**
- * Hello world!
- *
- */
 public class App 
 {
     public static void main( String[] args )
     {
         MarsRover marsRover = new MarsRover(5, 5);
         
-        marsRover.addRover(1,2,'N');
+        //adding rover#1
+        try {
+			marsRover.addRover(1,2,'N');
+			  marsRover.sendCommand("LMLMLMLMM");
+		} catch (CreatingRoverException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+        
+        try{        
+        	marsRover.addRover(3,3,'E');
+        	marsRover.sendCommand("MMRMMRMRRM");
+        }catch (CreatingRoverException e) {
+        	e.printStackTrace();
+		}
+        marsRover.getFinalPositions();
         
     }
 }
