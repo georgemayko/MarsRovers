d2b78f8e1d0b2abc1621282315a71ad0320de4a3
George Mayko <mayko.george@gmail.com>
2012-10-13 20:19:52 -0300

    Creating exception for handle the error on rovers creation

---
+++ src/main/java/br/com/gm/exception/CreatingRoverException.java
@@ -0,0 +1,17 @@
+package br.com.gm.exception;
+
+public class CreatingRoverException extends Exception {
+	
+	
+	private static final long serialVersionUID = -7250433822032634281L;
+
+	
+	public CreatingRoverException() {
+	}
+
+	public CreatingRoverException(String message) {
+		super(message);
+	}
+	
+
+}
