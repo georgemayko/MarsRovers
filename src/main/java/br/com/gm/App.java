6b8f4c2d5c511d36d8d8502081a03df63303ae92
George Mayko <mayko.george@gmail.com>
2012-10-13 23:13:45 -0300

    Refactoring App to only listen IvalidMarsRoverPosition

--- src/main/java/br/com/gm/App.java
+++ src/main/java/br/com/gm/App.java
@@ -1,30 +1,33 @@
 package br.com.gm;
 
-import br.com.gm.exception.CreatingRoverException;
+import br.com.gm.exception.InvalidMarsRoverPosition;
 import br.com.gm.model.MarsRover;
 
 public class App 
 {
-    public static void main( String[] args )
-    {
-        MarsRover marsRover = new MarsRover(5, 5);
-        
-        //adding rover#1
-        try {
+	public static void main( String[] args )
+	{
+		MarsRover marsRover = new MarsRover(5, 5);
+
+		//adding rover#1
+		try {
 			marsRover.addRover(1,2,'N');
-			  marsRover.sendCommand("LMLMLMLMM");
-		} catch (CreatingRoverException e) {
+			marsRover.sendCommand("LMLMLMLMM");
+		} catch (InvalidMarsRoverPosition e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-        
-        try{        
-        	marsRover.addRover(3,3,'E');
-        	marsRover.sendCommand("MMRMMRMRRM");
-        }catch (CreatingRoverException e) {
-        	e.printStackTrace();
+
+
+
+		try{        
+			marsRover.addRover(3,3,'E');
+			marsRover.sendCommand("MMRMMRMRRM");
+		} catch (InvalidMarsRoverPosition e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		}
-        marsRover.getFinalPositions();
-        
-    }
+		marsRover.getFinalPositions();
+
+	}
 }
