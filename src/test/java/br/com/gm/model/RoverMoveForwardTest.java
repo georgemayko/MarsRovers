3b8d0c27b1215dc88b43fdf8c6e09b03e7d1ccb0
George Mayko <mayko.george@gmail.com>
2012-10-14 14:03:48 -0300

    Creating tests to move out of plateu

--- src/test/java/br/com/gm/model/RoverMoveForwardTest.java
+++ src/test/java/br/com/gm/model/RoverMoveForwardTest.java
@@ -17,7 +17,7 @@ public class RoverMoveForwardTest {
 	
 	@Before
 	public void constructRover() throws CreatingRoverException, SecurityException, NoSuchMethodException{
-		this.rover = new Rover(0, 0, 'N', new MarsRover(0, 0));
+		
 		moveForward = Rover.class.getDeclaredMethod("moveForward",Orientation.class);
 		moveForward.setAccessible(true);
 	} 
@@ -25,6 +25,53 @@ public class RoverMoveForwardTest {
 	@Test(expected = MovementInvalidException.class)
 	public void movingForwardToNorthOutOfPlateuTest() throws Throwable {
 		try {
+			//rover on max Y coordenate
+			this.rover = new Rover(1, 1, 'N', new MarsRover(1, 1));
+			moveForward.invoke(rover, rover.getOrientation());
+		} catch (IllegalArgumentException e) {
+			e.getCause();
+		} catch (IllegalAccessException e) {
+			e.getCause();
+		} catch (InvocationTargetException e) {
+			throw e.getCause();
+		}
+	}
+	
+	@Test(expected = MovementInvalidException.class)
+	public void movingForwardToEastOutOfPlateuTest() throws Throwable {
+		try {
+			//rover on max X coordenate
+			this.rover = new Rover(1, 0, 'E', new MarsRover(1, 1));
+			moveForward.invoke(rover, rover.getOrientation());
+		} catch (IllegalArgumentException e) {
+			e.getCause();
+		} catch (IllegalAccessException e) {
+			e.getCause();
+		} catch (InvocationTargetException e) {
+			throw e.getCause();
+		}
+	}
+	
+	@Test(expected = MovementInvalidException.class)
+	public void movingForwardToSouthOutOfPlateuTest() throws Throwable {
+		try {
+			//rover on min Y coordenate
+			this.rover = new Rover(1, 0, 'S', new MarsRover(1, 1));
+			moveForward.invoke(rover, rover.getOrientation());
+		} catch (IllegalArgumentException e) {
+			e.getCause();
+		} catch (IllegalAccessException e) {
+			e.getCause();
+		} catch (InvocationTargetException e) {
+			throw e.getCause();
+		}
+	}
+	
+	@Test(expected = MovementInvalidException.class)
+	public void movingForwardToWestOutOfPlateuTest() throws Throwable {
+		try {
+			//rover on min X coordenate
+			this.rover = new Rover(0, 1, 'W', new MarsRover(1, 1));
 			moveForward.invoke(rover, rover.getOrientation());
 		} catch (IllegalArgumentException e) {
 			e.getCause();
